c1edf0bfb7a270a7fa40f792e5354cace34ce787
Harsh Gajera <harsh.gajera17@gmail.com>
2022-08-27 13:45:50 +0530

    done changes

--- pass.js
+++ pass.js
@@ -1,61 +1,7 @@
-a = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-A = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-no = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-sym = ["!", "@", "#", "$", "%", "^", "&", "*", "*", "(", ")"];
+const a = "abcdefghijklmnopqrstuvwxyz";
+const A = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const no = "1234567890";
+const sym = "!@#$%^&*()_-+=|/";
 
 let input = document.querySelector("form");
 input.addEventListener("change", function () {
